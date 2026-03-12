c48426db5d0c235cc57519dddc38d8e132ee8ea9
Michael La Plante <michael@michaellaplante.com>
2026-03-12 15:56:09 -0400

    feat: wire all 7 scenes with transitions into main Video composition

--- video/src/Root.tsx
+++ video/src/Root.tsx
@@ -3,7 +3,9 @@ import { Composition } from 'remotion';
 import { Video } from './Video';
 import { VIDEO, SCENE_DURATIONS } from './styles/theme';
 
-const totalDuration = Object.values(SCENE_DURATIONS).reduce((a, b) => a + b, 0);
+const TRANSITION_FRAMES = 15;
+const NUM_TRANSITIONS = 6;
+const totalDuration = Object.values(SCENE_DURATIONS).reduce((a, b) => a + b, 0) + (TRANSITION_FRAMES * NUM_TRANSITIONS);
 
 export const RemotionRoot: React.FC = () => {
   return (
